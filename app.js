f65c26d3cdd754725f97d7d790937bc3c408fae9
Mhd Sinan C <mhdsinanc313@gmail.com>
2024-09-17 14:25:12 +0530

    fix issues

--- app.js
+++ app.js
@@ -160,7 +160,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error.ejs', { err })
 })
 
-
-app.listen(3000, () => {
-    console.log('Serving on PORT 3000');
+const port = process.env.PORT || 3000 ;
+app.listen(port, () => {
+    console.log(`Serving on PORT ${port}`);
 })
